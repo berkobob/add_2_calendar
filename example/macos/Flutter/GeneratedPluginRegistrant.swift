fbf7adf715b96179127a70dd229a41421d430698
Antoine Lever <antoinelever@me.com>
2023-01-08 12:13:14 +0000

    found loose branch

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import add_2_calendar
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  Add2CalendarPlugin.register(with: registry.registrar(forPlugin: "Add2CalendarPlugin"))
+}
